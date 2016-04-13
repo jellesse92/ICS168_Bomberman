d1ec810206c4ed1622f64cab48a0554ba1f38ed8
achongco <achongco@uci.edu>
2016-04-12 20:48:29 -0700

    Ham Ham Power Ups!

--- Bomberman/Assets/Scripts/BombDrop.cs
+++ Bomberman/Assets/Scripts/BombDrop.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class BombDrop : MonoBehaviour {
     public GameObject bombPrefab;
+
+    int bombs_placed = 0;
     
     void Update () {
         if (Input.GetKeyDown(KeyCode.Space)) {
@@ -10,8 +12,20 @@ public class BombDrop : MonoBehaviour {
             pos.x = Mathf.Round(pos.x);
             pos.y = Mathf.Round(pos.y);
 
-            GameObject bomb = (GameObject)Instantiate(bombPrefab, pos, Quaternion.identity);
-            bomb.GetComponent<Bomb>().player = GetComponent<PlayerController>().player;
+            if(bombs_placed < GetComponent<PlayerController>().maxBombsAllowed)
+            {
+                StartCoroutine("PlacedBomb");
+                GameObject bomb = (GameObject)Instantiate(bombPrefab, pos, Quaternion.identity);
+                bomb.GetComponent<Bomb>().player = GetComponent<PlayerController>().player;
+            }
+
         }
+    }
+
+    IEnumerator PlacedBomb()
+    {
+        bombs_placed++;
+        yield return new WaitForSeconds(3.0f);
+        bombs_placed--;
     }
 }
---
+++ Bomberman/Assets/Scripts/ExplosionScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionScript : MonoBehaviour {
+
+    public int player = -1;
+    public GameObject[] ExplosionParticles;
+
+	public void Resize(float life)
+    {
+        for (int i = 0; i < 4; i++)
+            ExplosionParticles[i].GetComponent<ParticleSystem>().startLifetime = life;
+    }
+}
