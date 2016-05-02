46ff2b5c9e785e5699cc056337cd3c5275ceea50
Matthew Ruiz <mpruiz@uci.edu>
2016-05-01 23:21:47 -0700

    Added a new script to play the background music for! Attached to an object in the scene called Music Box I believe.

---
+++ Bomberman/Assets/Scripts/Music.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+	public AudioClip backgroundMusic;
+	// Use this for initialization
+	void Start () {
+	
+		AudioSource.PlayClipAtPoint (backgroundMusic, transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
