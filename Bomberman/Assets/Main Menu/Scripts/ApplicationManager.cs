e9e7857c41a3854a794ee6b4e750f02f66a9ed19
jellesse92 <jehunter@uci.edu>
2016-05-18 11:07:46 -0700

    Adjustment to Variable that doesnt work across computers

--- Bomberman/Assets/Main Menu/Scripts/ApplicationManager.cs
+++ Bomberman/Assets/Main Menu/Scripts/ApplicationManager.cs
@@ -56,7 +56,7 @@ public class ApplicationManager : MonoBehaviour {
     public bool data_error = false;
     public bool connected = false;
     public int port = 7777;
-    string myip = Network.player.ipAddress;
+    string myip = "";
     public string address = "52.26.63.128";
     public string username = "";
     string _lastSentMsg;
