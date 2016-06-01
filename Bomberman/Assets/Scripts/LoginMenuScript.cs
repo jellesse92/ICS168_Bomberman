38e193362a15f68635a683d350fa41c5e17aeefb
achongco <achongco@uci.edu>
2016-05-31 21:05:15 -0700

    More joining game stuff
    with MATT and JAYSEN

--- Bomberman/Assets/Scripts/LoginMenuScript.cs
+++ Bomberman/Assets/Scripts/LoginMenuScript.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class LoginMenuScript : MonoBehaviour {
 
+
+
     public GameObject[] gameSelectButtons;
 
     ApplicationManager appManageScript;
@@ -23,31 +25,30 @@ public class LoginMenuScript : MonoBehaviour {
     public void FindGames()
     {
         string serverRes = "";
+
         foreach (GameObject button in gameSelectButtons)
             button.SetActive(false);
+
         serverRes = appManageScript.GetServerResponse("5:");
-        if(serverRes != "NONE")
+
+        if(serverRes.Substring(0,4) != "NONE")
         {
-            string[] games;
 
-            Debug.Log("WHOLE!: " + serverRes);
-            
-            games = serverRes.Split(new[] { ' '});
-            if (games.Length > 0)
-                Debug.Log(games[0]);
-            if (games.Length > 1)
-                Debug.Log(games[1]);
-            
-            //string[] temp = games[0].Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
-            //Debug.Log("NAME: " + temp[0]);
+            Debug.Log("WHY?!!" + serverRes);
+            string[] gamesAvailable;
 
+            gamesAvailable = serverRes.Trim().Split(new[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
 
+            for(int i = 0; i < gamesAvailable.Length; i++)
+            {
+                if (i >= gameSelectButtons.Length || gameSelectButtons.Length <= 0)
+                    return;
 
+                string[] gameInfo = gamesAvailable[i].Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
 
-
-            //temp = serverRes.Split(new[] { ':'}, StringSplitOptions.RemoveEmptyEntries);
-
-
+                gameSelectButtons[i].transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = gameInfo[0];
+                gameSelectButtons[i].SetActive(true);
+            }
         }
 
     }
